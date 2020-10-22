4e10282606fbdcbdcf7ecf68616025781fc17ee4
Mar Alguacil <maralguacil@correo.ugr.es>
2020-10-22 21:59:10 +0200

    Upload files

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "./tmp"
 
 
 # Define EndPoints
@@ -11,6 +14,14 @@ def home():
 @app.route('/demo', strict_slashes=False)
 def demo():
     return render_template("demo.html")
+
+@app.route('/uploader', methods=['POST'])
+def uploader():
+    if request.method == "POST":
+        f = request.files['image']
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return "Imagen subida exitosamente"
 
 
 if __name__ == '__main__':
