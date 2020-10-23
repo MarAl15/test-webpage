e2abc2da2311ef72708c9f3be55572bb23af4842
Mar Alguacil <maralguacil@correo.ugr.es>
2020-10-23 20:22:13 +0200

    Preview image before uploading

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,14 @@
 document.getElementById("add-segmap").onclick = function() {
     document.getElementById("add-new-segmap").click();
 };
+
+document.getElementById("add-new-segmap").onchange = function() {
+    file = this.files[0];
+
+    if (['image/jpeg', 'image/png'].indexOf(file.type) != -1)
+        document.getElementById('span-segmap').outerHTML =
+                '<img src="' + URL.createObjectURL(file) + '">'
+    else
+        alert('File not supported. Allowed image types are: png, jpg, jpeg.')
+
+};
