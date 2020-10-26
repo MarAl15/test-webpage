464a161b99976a6ac1a2bd398f3c9371cbdcc816
Mar Alguacil <maralguacil@correo.ugr.es>
2020-10-26 21:23:04 +0100

    Compute synthesized image. Generate unique filename.

--- index.py
+++ index.py
@@ -24,47 +24,33 @@ tester = TesterOne(args)
 def home():
     return render_template("home.html")
 
-filename_segmap = None
-filename_style = None
-filename_fake = None
 
 @app.route('/demo', methods=['GET', 'POST'])
 def demo():
-    global filename_segmap, filename_style, filename_fake
     if request.method == "POST":
-        if 'file' not in request.files:
+        if 'file_segmap' not in request.files or 'file_style' not in request.files:
             flash('No file part.')
             return redirect(request.url)
-        f = request.files['file']
-        if f.filename == '':
-            flash('No image selected for uploading.')
+        f_segmap = request.files['file_segmap']
+        f_style = request.files['file_style']
+        if f_segmap.filename == '' or f_style.filename == '':
+            flash('No segmentation map or style image selected.')
             return redirect(request.url)
 
         # filename = f.filename
-        filename, file_extension = os.path.splitext(secure_filename(f.filename))
-        filename += '_'+''.join(random.choices(string.ascii_letters, k=10))+file_extension
+        filename_segmap, file_extension_segmap = generate_filename(f_segmap.filename)
+        filename_style, file_extension_style = generate_filename(f_style.filename)
 
-        if file_extension.lower() in [".jpeg", ".jpg", ".png"]:
-            image_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            f.save(image_file)
+        if file_extension_segmap.lower() in [".jpeg", ".jpg", ".png"] and \
+           file_extension_style.lower() in [".jpeg", ".jpg", ".png"]:
+            segmap_file = os.path.join(app.config['UPLOAD_FOLDER'], filename_segmap)
+            styime_file = os.path.join(app.config['UPLOAD_FOLDER'], filename_style)
+            f_segmap.save(segmap_file)
+            f_style.save(styime_file)
 
-            flash('Image successfully uploaded and displayed.')
+            flash('Images successfully displayed.')
 
-            if request.form['submit_button'] == "Upload Segmentation Map":
-                tester.preprocess_image(image_file, num_channels=1,
-                                       resize_method=tf.image.ResizeMethod.NEAREST_NEIGHBOR,
-                                       normalize=False)
-                if filename_segmap is not None and filename_segmap!=filename:
-                    os.remove(image_file, normalize=False)
-                filename_segmap = filename
-            elif request.form['submit_button'] == "Upload Style Filter":
-                tester.preprocess_image(image_file)
-                if filename_style is not None and filename_style!=filename:
-                    os.remove(image_file)
-                filename_style = filename
-            if filename_segmap is not None and filename_style is not None:
-                filename_fake = tester.generate_fake(os.path.join(app.config['UPLOAD_FOLDER'], filename_segmap),
-                                     os.path.join(app.config['UPLOAD_FOLDER'], filename_style))
+            filename_fake = tester.generate_fake(segmap_file, styime_file)
 
             return render_template('demo.html', filename_segmap=filename_segmap, filename_style=filename_style,
                                                 filename_fake=filename_fake)
@@ -72,20 +58,39 @@ def demo():
         flash('No valid images were found. Allowed image types are: png, jpg, jpeg.')
         return redirect(request.url)
 
-    # Remove files
-    if filename_segmap is not None:
-        os.remove(os.path.join(app.config['UPLOAD_FOLDER'],filename_segmap))
-        filename_segmap = None
-    if filename_style is not None:
-        os.remove(os.path.join(app.config['UPLOAD_FOLDER'],filename_style))
-        filename_style = None
-
     return render_template("demo.html")
 
 
 @app.route('/display/<filename>')
 def display_image(filename):
     return redirect(url_for('static', filename='tmp/' + filename), code=301)
+
+
+
+# https://stackoverflow.com/questions/279561/what-is-the-python-equivalent-of-static-variables-inside-a-function
+def static_vars(**kwargs):
+    def decorate(func):
+        for k in kwargs:
+            setattr(func, k, kwargs[k])
+        return func
+    return decorate
+
+@static_vars(n_random_letters=5)
+def generate_filename(filename):
+    """Creates a unique filename by adding a random string of letters."""
+    file_name, file_extension = os.path.splitext(secure_filename(filename))
+
+    for i in range(5):
+        filename = file_name + '_'+''.join(random.choices(string.ascii_letters, k=generate_filename.n_random_letters))+file_extension
+        if not os.path.isfile(filename):
+            return filename, file_extension
+
+    # Change the length of the random string if all the generated possibilities exist
+    generate_filename.n_random_letters+=1
+    return file_name + '_'+''.join(random.choices(string.ascii_letters, k=generate_filename.n_random_letters))+file_extension, file_extension
+
+
+
 
 
 if __name__ == '__main__':
