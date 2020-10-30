1e3be4394c0b62c6be0ed3bc9fec11cc6039af96
Mar Alguacil <maralguacil@correo.ugr.es>
2020-10-30 16:58:33 +0100

    Draw and change color

--- static/js/version2.js
+++ static/js/version2.js
@@ -18,6 +18,81 @@ document.getElementById("add-new-style-v2").onchange = function() {
 /*
  * PAINT
  */
-let canvas = document.getElementById("paint-segmap"),
-    ctx = canvas.getContext("2d"),
-    width = canvas.width, height = canvas.height;
+let canvas = document.getElementById('paint-segmap'), // capturar canvas
+    ctx = canvas.getContext('2d'), // contexto - nos permite dibujar sobre este objeto, manipularlo
+    x=0, y=0,
+    drawing = false,
+    color = 'black',
+    stroke_width = 1;
+
+ctx.canvas.width  = canvas.offsetWidth;
+ctx.canvas.height = canvas.offsetHeight;
+
+window.onresize = function() {
+    // Make our in-memory canvas
+    let inMemCanvas = document.createElement('canvas'),
+        inMemCtx = inMemCanvas.getContext('2d');
+    // Save image
+    inMemCanvas.width = canvas.width;
+    inMemCanvas.height = canvas.height;
+    inMemCtx.drawImage(canvas, 0, 0);
+    // Redraw the image back on canvas
+    ctx.canvas.width  = canvas.offsetWidth;
+    ctx.canvas.height = canvas.offsetHeight;
+    ctx.drawImage(inMemCanvas, 0, 0, inMemCanvas.width, inMemCanvas.height, 0, 0, canvas.width, canvas.height);
+}
+
+
+// Capturamos el primer evento que sucede - cuando el usuario hace click sobre el canvas
+// e -> datos de donde el usuario dio click en la pantalla
+canvas.addEventListener('mousedown', function(e) {
+    // conseguimos la coordenadas correspondientes en el canvas
+    x = e.pageX - canvas.offsetLeft; // posición en x donde el usuario dio click en la pantalla - la posicion del canvas
+    y = e.pageY - canvas.offsetTop; // posición en y donde el usuario dio click en la pantalla - la posicion del canvas
+
+    drawing = true;
+});
+
+// Movemos el ratón
+// Dispara realmente la función que va a dibujar
+canvas.addEventListener('mousemove', function(e) {
+    if (drawing) {
+        // x,y -> punto inicial
+        // -> puntos en este momoento donde se encuentra el ratón, donde llegó
+        let x_prev = x,
+            y_prev = y;
+
+        x = e.pageX - canvas.offsetLeft;
+        y = e.pageY - canvas.offsetTop;
+        draw(x_prev,y_prev, x,y);
+    }
+});
+
+
+// Quita el click del ratón
+canvas.addEventListener('mouseup', function(e) {
+    if (drawing) {
+        // x,y -> punto inicial
+        // -> puntos en este momoento donde se encuentra el ratón, donde llegó
+        draw(x, y,
+             e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+
+        drawing = false;
+        x = 0, y = 0;
+    }
+});
+
+
+function change_color(new_color) {
+    color = new_color;
+}
+
+function draw(x1, y1, x2, y2) {
+    ctx.beginPath(); // nueva ruta
+    ctx.strokeStyle = color; // Color
+    ctx.lineWidth = stroke_width; // Grosor de la linea
+    ctx.moveTo(x1, y1); // mover a la coordenada inicial
+    ctx.lineTo(x2, y2); // dibujar una línea
+    ctx.stroke(); // va a realizar una simple línea, no va a rellenar áreas
+    ctx.closePath(); // cerramos la ruta
+}
