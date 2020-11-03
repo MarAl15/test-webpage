6f33f10612bb483c0922c068f0c0a35f3f5854f0
Mar Alguacil <maralguacil@correo.ugr.es>
2020-11-03 16:00:24 +0100

    Extract common code

--- index.py
+++ index.py
@@ -48,53 +48,11 @@ def demo():
 
         if file_extension_segmap.lower() in [".jpeg", ".jpg", ".png"] and \
            file_extension_style.lower() in [".jpeg", ".jpg", ".png"]:
-            #https://stackoverflow.com/questions/47515243/reading-image-file-file-storage-object-using-cv2
-            # Read image file string data
-            segmap = f_segmap.read()
-            styimg = f_style.read()
-
-            # Convert string data to numpy array
-            segmap = np.fromstring(segmap, np.uint8)
-            styimg = np.fromstring(styimg, np.uint8)
-
-            # Convert numpy array to image
-            segmap = cv2.imdecode(segmap, cv2.IMREAD_GRAYSCALE)
-            styimg = cv2.imdecode(styimg, cv2.IMREAD_COLOR)
-            ######
-
-            # OpenCV likes to treat images as BGR instead of RGB. We need to swap layers
-            #https://docs.opencv.org/3.4/d4/da8/group__imgcodecs.html#gga61d9b0126a3e57d9277ac48327799c80aeddd67043ed0df14f9d9a4e66d2b0708
-            styimg = cv2.cvtColor(styimg, cv2.COLOR_BGR2RGB)
-
-            #########################
-            #   SYNTHESIZED IMAGE
-            #########################
-            # Create synthesized image
-            fake_img = tester.generate_fake(segmap, styimg)[0].numpy()
-
-            # Denormalize
-            fake_img = (fake_img + 1) * 127.5
-
-            # Convert numpy array of type float64 to type uint8
-            fake_img = fake_img.astype(np.uint8)
-
-            #####
-            # https://stackoverflow.com/questions/56946969/how-to-send-an-image-directly-from-flask-server-to-html
-            # Convert numpy array to PIL Image
-            fake_img = Image.fromarray(fake_img)
-
-            # Create file-object in memory
-            file_object = io.BytesIO()
-
-            # Write PNG in file-object
-            fake_img.save(file_object, 'PNG')
-
-            # Move to beginning of file so `send_file()` it will read from start
-            file_object.seek(0)
+            segmap = file2img(f_segmap, color_mode=cv2.IMREAD_GRAYSCALE)
+            styimg = file2img(f_style)
 
             flash('Images successfully displayed.')
-            return send_file(file_object, mimetype='image/PNG')
-            #####
+            return send_file(generate_fake(segmap, styimg), mimetype='image/PNG')
 
         flash('No valid images were found. Allowed image types are: png, jpg, jpeg.')
         return redirect(request.url)
@@ -105,11 +63,9 @@ def demo():
 @cross_origin()
 def demo_v2():
     if request.method == "POST":
-        # STYLE IMAGE
         if 'file_style' not in request.files:
             flash('No file part.')
             return redirect(request.url)
-        #####
         f_style = request.files['file_style']
         if f_style.filename == '':
             flash('No style image selected.')
@@ -120,19 +76,7 @@ def demo_v2():
             ###################
             #   STYLE IMAGE
             ###################
-            #https://stackoverflow.com/questions/47515243/reading-image-file-file-storage-object-using-cv2
-            # Read style image file string data
-            styimg = f_style.read()
-
-            # Convert string data to numpy array
-            styimg = np.fromstring(styimg, np.uint8)
-
-            # Convert numpy array to image
-            styimg = cv2.imdecode(styimg, cv2.IMREAD_UNCHANGED)
-            ######
-
-            # OpenCV likes to treat images as BGR instead of RGB. We need to swap layers
-            styimg = cv2.cvtColor(styimg, cv2.COLOR_BGR2RGB)
+            styimg = file2img(f_style)
 
             ########################
             #   SEGMENTATION MAP
@@ -143,47 +87,71 @@ def demo_v2():
             # Convert the data URL to an OpenCV image
             segmap = uri2img(b64_string)
 
-
             #########################
             #   SYNTHESIZED IMAGE
             #########################
-            # Create synthesized image
-            fake_img = tester.generate_fake(segmap, styimg)[0].numpy()
-
-            # Denormalize
-            fake_img = (fake_img + 1) * 127.5
-
-            # Convert numpy array of type float64 to type uint8
-            fake_img = fake_img.astype(np.uint8)
-
-            #####
-            # https://stackoverflow.com/questions/56946969/how-to-send-an-image-directly-from-flask-server-to-html
-            # Convert numpy array to PIL Image
-            fake_img = Image.fromarray(fake_img)
-
-            # Create file-object in memory
-            file_object = io.BytesIO()
-
-            # Write PNG in file-object
-            fake_img.save(file_object, 'PNG')
-
-            # Move to beginning of file so `send_file()` it will read from start
-            file_object.seek(0)
-
             flash('Images successfully displayed.')
-            return send_file(file_object, mimetype='image/PNG')
-            #####
+            return send_file(generate_fake(segmap, styimg), mimetype='image/PNG')
 
         flash('No valid images were found. Allowed image types are: png, jpg, jpeg.')
         return redirect(request.url)
 
     return render_template("demov2.html")
 
+
+def generate_fake(segmap, styimg):
+    """Creates a fake image from the segmentation map and the style image, and
+      returns a file-object in memory with the synthesized image.
+    """
+    # Create synthesized image
+    fake_img = tester.generate_fake(segmap, styimg)[0].numpy()
+
+    # Denormalize
+    fake_img = (fake_img + 1) * 127.5
+
+    # Convert numpy array of type float64 to type uint8
+    fake_img = fake_img.astype(np.uint8)
+
+    # https://stackoverflow.com/questions/56946969/how-to-send-an-image-directly-from-flask-server-to-html
+    # Convert numpy array to PIL Image
+    fake_img = Image.fromarray(fake_img)
+
+    # Create file-object in memory
+    file_object = io.BytesIO()
+
+    # Write PNG in file-object
+    fake_img.save(file_object, 'PNG')
+
+    # Move to beginning of file so `send_file()` it will read from start
+    file_object.seek(0)
+
+    return file_object
+
+def file2img(file, color_mode=cv2.IMREAD_COLOR):
+    """Converts an image file into an OpenCV image.
+    Main credit: https://stackoverflow.com/questions/47515243/reading-image-file-file-storage-object-using-cv2
+    """
+    # Read image file string data
+    img = file.read()
+
+    # Convert string data to numpy array
+    img = np.fromstring(img, np.uint8)
+
+    # Convert numpy array to image
+    img = cv2.imdecode(img, color_mode)
+
+    if color_mode == cv2.IMREAD_COLOR:
+        # OpenCV likes to treat images as BGR instead of RGB. We need to swap layers
+        #https://docs.opencv.org/3.4/d4/da8/group__imgcodecs.html#gga61d9b0126a3e57d9277ac48327799c80aeddd67043ed0df14f9d9a4e66d2b0708
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    return img
+
+
 def uri2img(b64_string):
-    """Convert a data URL to an OpenCV image.
+    """Converts a data URL into an OpenCV image.
     Credit: https://stackoverflow.com/a/54205640/2415512
     """
-
     encoded_data = b64_string.split(',')[1]
     nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
 
